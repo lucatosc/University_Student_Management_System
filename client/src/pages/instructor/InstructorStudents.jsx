d783d3d4a2d8a768ed181fdacafaf9a7fa867129
lucatosc <toscano6210@gmail.com>
2024-07-20 20:10:55 +0500

    students according to course update

--- client/src/pages/instructor/InstructorStudents.jsx
+++ client/src/pages/instructor/InstructorStudents.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import InstructorLayout from '../../layouts/InstructorLayout';
-import { courseEndpoints } from "../../api/endpoints/courseEndpoints";
-import { fetchResponse } from "../../api/service";
-import { toast } from "react-toastify";
-import { toastErrorObject } from "../../utility/toasts";
-import LoadingSpinner from "../../components/spinners/LoadingSpinner";
-import DynamicTable from "../../components/tables/DynamicTable";
+import { courseEndpoints } from '../../api/endpoints/courseEndpoints';
+import { fetchResponse } from '../../api/service';
+import { toast } from 'react-toastify';
+import { toastErrorObject } from '../../utility/toasts';
+import LoadingSpinner from '../../components/spinners/LoadingSpinner';
+import DynamicTable from '../../components/tables/DynamicTable';
+import SelectField from '../../components/inputs/SelectField';
+import moment from 'moment';
 
 export default function InstructorStudents() {
-  const instructorId = JSON.parse(localStorage.getItem("instructor"))._id;
+  const uniqueCourseIds = {};
+  const instructorId = JSON.parse(localStorage.getItem('instructor'))._id;
 
   const [students, setStudents] = useState([]);
+  const [temporarySelection, setTemporarySelection] = useState({
+    date: moment(Date.now()).format('YYYY-MM-DD'),
+    course: '',
+  });
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -28,8 +35,13 @@ export default function InstructorStudents() {
           setIsLoading(false);
           return;
         }
-        console.log("Log data", resData);
-        setStudents(resData?.map(student => ({ ...student, name: student.fname + " " + student.lname })));
+        console.log('Log data', resData);
+        setStudents(
+          resData?.map((student) => ({
+            ...student,
+            name: student.fname + ' ' + student.lname,
+          }))
+        );
         setIsLoading(false);
       } catch (error) {
         console.log(error);
@@ -43,17 +55,38 @@ export default function InstructorStudents() {
 
   return (
     <InstructorLayout>
+      <div className='mb-4'>
+        <SelectField
+          label={'Select Course'}
+          options={students
+            .filter((attendance) => {
+              const courseId = attendance.courseId;
+              if (!uniqueCourseIds[courseId]) {
+                uniqueCourseIds[courseId] = true;
+                return true;
+              }
+              return false;
+            })
+            .map((attendance) => ({
+              value: attendance.courseId,
+              title: attendance.courseTitle,
+            }))}
+          value={temporarySelection?.course}
+          onChange={(event) =>
+            setTemporarySelection({
+              ...temporarySelection,
+              course: event.target.value,
+            })
+          }
+        />
+      </div>
       <DynamicTable
-        styles={"table-bordered"}
-        headers={[
-          "Roll Number",
-          "Name",
-          "Email Address",
-          "Course",
-          "Joining Date",
-        ]}
-        data={students}
-        dataAttributes={["rollNumber", "name", "email", "courseTitle", "createdAt"]}
+        styles={'table-bordered'}
+        headers={['Roll Number', 'Name', 'Email Address', 'Joining Date']}
+        data={students?.filter(
+          (student) => student?.courseId === temporarySelection.course
+        )}
+        dataAttributes={['rollNumber', 'name', 'email', 'createdAt']}
       />
     </InstructorLayout>
   );
