610ddf485d0353aa4a616708fdde93deb06b11a8
lucatosc <toscano6210@gmail.com>
2024-07-22 21:44:03 +0500

    home card my fix

--- client/src/components/cards/HomeCard.jsx
+++ client/src/components/cards/HomeCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function HomeCard({ image, alt, text, path }) {
   return (
-    <Link to={path} className="card bg-dark shadow border-2 border-secondary" style={{ width: "18rem", margin: "30px", borderRadius: "20px" }}>
+    <Link to={path} className="card bg-dark shadow border-2 border-secondary" style={{ width: "18rem", marginLeft: "30px", marginRight: "30px", borderRadius: "20px" }}>
       <img src={image} className="card-img" alt={alt} style={{ borderRadius: "18px" }} />
       <div className="card-img-overlay">
         <h5 className="card-title shadow-sm bg-white text-secondary ps-3 py-1 fw-bold" style={{ borderRadius: "20px", marginRight: "80px" }}>{text}</h5>
--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -10,7 +10,7 @@ export default function HomeLayout({ isLoading, children }) {
         <LoadingSpinner />
       ) : (
         <div
-          className='d-flex align-items-center justify-content-center flex-column flex-md-row'
+          className='d-flex align-items-center justify-content-center flex-column flex-md-row gap-4 my-5 my-md-0'
           style={{ minHeight: '91vh' }}
         >
           {children}
